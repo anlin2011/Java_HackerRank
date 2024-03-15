ceed9bef9086c90a8fcf84b29fbefe77727c0d95
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-15 16:44:54 +0530

    Add files via upload

---
+++ 3.Java If-Else/If_Else.java
@@ -0,0 +1,22 @@
+package HackerRank;
+import java.util.*;
+public class If_Else {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if(N%2 == 0) {
+			if(N>=2 && N<=5) {
+				System.out.println("Not Weird");
+			}
+			else if(N>=6 && N<=20) {
+				System.out.println("Weird");
+			}
+			else {
+				System.out.println("Not Weird");
+			}
+		}
+		else {
+			System.out.println("Weird");
+		}
+	}
+}
