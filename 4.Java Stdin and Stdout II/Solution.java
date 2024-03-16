770549d68e7fbb35109aabf32b6450dbb597e8a7
anlin2011 <155153468+anlin2011@users.noreply.github.com>
2024-03-16 15:32:50 +0530

    Update Solution.java

--- 4.Java Stdin and Stdout II/Solution.java
+++ 4.Java Stdin and Stdout II/Solution.java
@@ -1,9 +1,8 @@
 package HackerRank;
 import java.util.Scanner;
 public class Solution {
-
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
         double d = scan.nextDouble();
         scan.nextLine();
@@ -12,7 +11,5 @@ public class Solution {
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
-
 	}
-
 }
